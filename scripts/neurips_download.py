2c57493f990779ea4b5eebf8fbbe9862bb6c8cbd
Nemanja <n.rakicevic@imperial.ac.uk>
2021-02-11 14:07:49 +0000

    Updated 2020 data; Updated scripts

--- scripts/neurips_download.py
+++ scripts/neurips_download.py
@@ -45,15 +45,20 @@ if os.path.isfile('neurips_conf_data.pkl'):
         conf_data = pickle.load(handle)
 else:
     conf_data = {}
+
 # Initialisation
 nips_papers = 'https://papers.nips.cc/'
 html_text = requests.get(nips_papers).text
 soup = BeautifulSoup(html_text, 'html.parser')
+
+
 # Loop through all conference years
 all_conferences = [
     cc for cc in soup.find_all('li') if 'paper' in cc.a.get('href')]
 all_conferences = all_conferences[::-1]
-for cc in all_conferences[len(conf_data):]:
+
+offset_conf = len(conf_data)
+for cc in all_conferences[offset_conf:]:
     conf_link = urljoin(nips_papers, cc.a.get('href'))
     conf_year = conf_link.split('/')[-1]
     html_text = requests.get(conf_link).text
@@ -62,58 +67,91 @@ for cc in all_conferences[len(conf_data):]:
     # Loop through all current conference's papers
     print("\n\nProcessing: ", cc.a.contents[0])
     paper_list = []
+    offset_papers = len(paper_list)
     all_papers = [
         pp for pp in conf.find_all('li') if 'paper' in pp.a.get('href')]
-    for pi, pp in enumerate(all_papers):
+
+    for pi, pp in enumerate(all_papers[offset_papers:]):
+
         # Get paper info
         print(" - paper [{}/{}]: {}".format(
-            pi + 1, len(all_papers), pp.a.contents[0]))
+            pi + 1 + offset_papers, len(all_papers), pp.a.contents[0]))
         paper_link = urljoin(conf_link, pp.a.get('href'))
-        link_file = paper_link.replace('hash', 'file')
+        html_paper = requests.get(paper_link).text
+
         # Extract paper metadata
-        link_meta = link_file.replace('html', 'json')
-        link_meta = link_meta.replace('Abstract', 'Metadata')
-        html_text = requests.get(link_meta).text
-        if html_text == 'Resource Not Found':
+        if "Metadata" in html_paper:
+            link_file = paper_link.replace('hash', 'file')
+            link_meta = link_file.replace('html', 'json')
+            link_meta = link_meta.replace('Abstract', 'Metadata')
+            html_text = requests.get(link_meta).text
+            if html_text == 'Resource Not Found':
+                conf = BeautifulSoup(html_paper, 'html.parser')
+                abstract_text = conf.find(
+                    'h4',
+                    string='Abstract').next_sibling.next_sibling.contents[0]
+                abstract = None if abstract_text == 'Abstract Unavailable' \
+                    else abstract_text
+                abstract = abstract.replace('<p>', '')
+                abstract = abstract.replace('</p>', '')
+                abstract = abstract.replace('\n', ' ')
+                author_list = [
+                    {'given_name': aa.split(' ')[0],
+                     'family_name': aa.split(' ')[1],
+                     'institution': None}
+                    for aa in pp.i.contents[0].split(', ')]
+                paper_meta = {
+                    'title': str(pp.a.contents[0]),
+                    'authors': author_list,
+                    'abstract': abstract,
+                    'full_text': None}
+            else:
+                paper_meta = json.loads(html_text)
+                if 'full_text' in paper_meta.keys():
+                    paper_meta['full_text'] = paper_meta['full_text'].replace(
+                        '\n', ' ')
+        else:
+            # make nicer not copy
+            conf = BeautifulSoup(html_paper, 'html.parser')
+            abstract_text = conf.find(
+                'h4',
+                string='Abstract').next_sibling.next_sibling.contents[0]
+            abstract = None if abstract_text == 'Abstract Unavailable' \
+                else str(abstract_text)
+            abstract = abstract.replace('<p>', '')
+            abstract = abstract.replace('</p>', '')
+            abstract = abstract.replace('\n', ' ')
             author_list = [
                 {'given_name': aa.split(' ')[0],
                  'family_name': aa.split(' ')[1],
                  'institution': None} for aa in pp.i.contents[0].split(', ')]
             paper_meta = {
-                'title': pp.a.contents[0],
+                'title': str(pp.a.contents[0]),
                 'authors': author_list,
-                'abstract': None,
-                'full_text': None
-            }
-        else:
-            paper_meta = json.loads(html_text)
+                'abstract': abstract,
+                'full_text': None}
+
         # Extract paper supplemental
-        link_supplement = link_file.replace('html', 'zip')
-        link_supplement = link_supplement.replace('Abstract', 'Supplemental')
-        html_text = requests.get(link_supplement).text
-        if html_text == 'Resource Not Found':
-            has_zip = False
+        if "Supplemental" in html_paper:
+            has_supplement = True
         else:
-            has_zip = True
-        link_supplement = link_supplement.replace('zip', 'pdf')
-        html_text = requests.get(link_supplement).text
-        if html_text == 'Resource Not Found':
-            has_pdf = False
-        else:
-            has_pdf = True
-        has_supplement = has_pdf or has_zip
+            has_supplement = False
+
         # Extract paper reviews
-        link_review = link_file.replace('Abstract', 'Reviews')
-        html_text = requests.get(link_review).text
-        if html_text == 'Resource Not Found':
-            reviews = None
-        else:
+        if "Reviews" in html_paper:
+            link_file = paper_link.replace('hash', 'file')
+            link_review = link_file.replace('Abstract', 'Reviews')
+            html_text = requests.get(link_review).text
             reviews = get_reviews(html_text)
+        else:
+            reviews = None
+
         # Extract scholar citation data
         num_cit = get_num_citations(
             title=paper_meta['title'],
             authors=paper_meta['authors'],
             year=conf_year)
+
         # Update paper info
         paper_meta.update({
             'year': conf_year,
@@ -123,7 +161,10 @@ for cc in all_conferences[len(conf_data):]:
             'reviews': reviews,
             'has_supplement': has_supplement})
         paper_list.append(paper_meta)
-    # Update conference info
+
+    # Update conference info and save to file
     conf_data[conf_year] = paper_list
-    with open('neurips_conf_data.pkl', 'wb') as handle:
+    with open('../data/neurips_conf_data.dat', 'wb') as handle:
         pickle.dump(conf_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    # prevents losing data
+    os.system("mv ../data/neurips_conf_data.dat ../data/neurips_conf_data.pkl")
